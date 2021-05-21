21713b77afc07ba5c18aa1ac584c0298a37de1ee
Bodhisatta <techno.tips@gmail.com>
2021-05-21 13:24:23 +0530

    Modify app

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask("__name__")
 @app.route('/')
 @app.route('/hello/')
 def hello_world():
-    return 'Hello World!\n'
+    return 'This is EvolvFit DevOps Challenge\n'
 
 @app.route('/hello/<username>') # dynamic route
 def hello_user(username):
-    return 'Why Hello %s!\n' % username
+    return 'Here is me submitting %s!\n' % username
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')   
